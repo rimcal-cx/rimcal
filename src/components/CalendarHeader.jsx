29f80cf095e4e6a306c5ecf4c81eec1b5a18c504
SURAJIT05071198 <surajit@rimsys.io>
2022-09-16 17:24:48 +0530

    image updated

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -55,7 +55,7 @@ function CalendarHeader() {
   return (
     <header className='px-2 py-4 flex items-center'>
 
-     <img src="https://assets-global.website-files.com/6141b0efd8e31331808ba8a4/6141eab29483d621180040d1_Logo%20Dark.svg" alt="" className='mr-2 w-14 '/>
+     <img src="logo-name.png" alt="" className='mr-2 w-14 '/>
      <button onClick={reset} className='border rounded py-2 px-4 ml-5'>
       Today
      </button>
