11ba3a34c8753938bfa7884d24bebce402ffd1cd
SURAJIT05071198 <surajit@rimsys.io>
2022-09-16 11:48:47 +0530

    delete api fix

--- app/Services/Google/Calendar/GoogleCalendarDeleteService.php
+++ app/Services/Google/Calendar/GoogleCalendarDeleteService.php
@@ -5,18 +5,19 @@ namespace App\Services\Google\Calendar;
 use App\Models\Calendar;
 use App\Services\Google\GoogleAuthClient;
 use Exception;
-use Google\Service\Calendar\Calendar as GoogleCalendar;
+use Google\Service\Calendar as GoogleCalendar;
 use Illuminate\Support\Facades\DB;
 
 class GoogleCalendarDeleteService
 {
 
-    public function handle(Calendar $calendar): bool
+    public function handle($calendarId): bool
     {
         try{
             DB::beginTransaction();
             $client = (new GoogleAuthClient)->handle();
             $service = new GoogleCalendar($client);
+            $calendar = Calendar::find($calendarId);
             $service->events->delete('primary', $calendar->event_id);
             $calendar->attendees()->delete();
             $calendar->delete();
